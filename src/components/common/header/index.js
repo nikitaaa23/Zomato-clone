57d7fbb7bf5b8baa09237678b0293fd076ff8bae
Nikita <saininikita23@gmail.com>
2022-01-25 01:11:44 +0530

    landing page

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -1,8 +1,40 @@
 import React from "react";
 import './header.css';
 
-const Header = () =>{
-    return <div>Header</div>;
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header-content">
+                <div className="logo">
+                    <img src="https://b.zmtcdn.com/web_assets/8313a97515fcb0447d2d77c276532a511583262271.png" class="high-res-image" alt="" className="header-logo"></img>
+                </div>
+                <div className="main-text">Discover the best food & drinks in Jaipur</div>
+                <div className="location-searchbar">
+                    <div className="location">
+                        <div className="location-icon">
+                            <i class="fa fa-map-marker"></i>
+                        </div>
+                        <div className="location-text">
+                            <input type="text" placeholder="Jaipur" />
+                        </div>
+                        <div className="caret">
+                            <i class="fa fa-caret-down"></i>
+                        </div>
+                    </div>
+                    <div className="seperator"></div>
+                    <div className="searchbar">
+                        <div className="search-icon">
+                            <i class="fa fa-search"></i>
+                        </div>
+                        <div className="searchbar-input">
+                            <input type="text" placeholder="Search for a restaurant, cuisine or a dish" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
 };
 
 export default Header;
--- src/webPages/homePage/index.js
+++ src/webPages/homePage/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/common/header";
+import Footer from "../../components/common/footer";
 
 const HomePage  = () => {
     return (
         <div>
             <Header />
+            <Footer />
         </div>
     )
 };
