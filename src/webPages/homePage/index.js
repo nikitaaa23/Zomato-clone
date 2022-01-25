deee71917eb9e1258127ca54fb3c8d2207620d6a
Nikita <saininikita23@gmail.com>
2022-01-25 15:21:29 +0530

    navbar

--- src/webPages/homePage/index.js
+++ src/webPages/homePage/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from '../../components/header';
 import Footer from "../../components/footer";
+import Navbar from "../../components/Navbar";
 
 import './index.css';
 
 const HomePage = () => {
     return (
         <div>
+            <Navbar />
             <Header/>
             <Footer/>
         </div>
