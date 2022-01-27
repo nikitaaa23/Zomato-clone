956af2a818e682082ae23ab1fbf2446d9012def7
Nikita <saininikita23@gmail.com>
2022-01-27 17:08:36 +0530

    commit

--- src/components/MobileApp/index.js
+++ src/components/MobileApp/index.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import'./mobileapp.css';
 
 const MobileApp = () => {
+    const [radio, setRadio] = useState("email");
+
     return(
         <div className="main">
             <div className="inner">
-            <img src="https://b.zmtcdn.com/data/pro/5722d9e687511d79616bcab92470734e1596187147.png" />
+                <div className="phone-image">
+                <img src="https://b.zmtcdn.com/data/pro/5722d9e687511d79616bcab92470734e1596187147.png" />
+                </div>
             <div className = "text-app">
             <h2>Get the Zomato App</h2>
+            <p>
+                We will send you a link, open it on your phone to download the app
+            </p>
+            <div className="radio-button">
+                <form action ="/">
+                    <input type="radio" id ="email" name="btn" value="email" checked = {radio==="email"} onChange={(e) => {setRadio(e.target.value)}}/>
+                    <label for="email">Email</label>
+                    <input type="radio" id ="phone" name="btn" value="phone" checked = {radio==="phone"} onChange={(e) => {setRadio(e.target.value)}}/>
+                    <label for="email">Phone</label>
+                </form>
+                </div>
+                <div className="input-bar">
+                    {radio == "email" ? <input type = "email" placeholder="Email" /> : <input type = "phone" placeholder="Phone" /> }
+                    <button>Share App Link</button>
+                </div>
+                <p className="download-text">Download app from</p>
+                <div className="download-source">
+                <img src="https://b.zmtcdn.com/data/webuikit/9f0c85a5e33adb783fa0aef667075f9e1556003622.png" />
+                <img src="https://b.zmtcdn.com/data/webuikit/23e930757c3df49840c482a8638bf5c31556001144.png"/>
+                </div>
             </div>
             </div>
         </div>
-    )
+    );
 };
 
 export default MobileApp;
